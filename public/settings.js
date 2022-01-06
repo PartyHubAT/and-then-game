f7608f5c7dc998a82074a5a7e309e34c96166b5e
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 11:43:02 +0100

    Add texts-per-player setting

--- public/settings.js
+++ public/settings.js
@@ -1,3 +1,15 @@
 ﻿module.exports = {
-  defaultValues: {},
+  form: {
+    textsPerPlayer: {
+      type: Number,
+      displayName: "Texts per player",
+      label: "How many texts will each player write?",
+      min: 1,
+      max: 10,
+      required: true,
+    },
+  },
+  defaultValues: {
+    textsPerPlayer: 1,
+  },
 };
