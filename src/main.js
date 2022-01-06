a122da207727f194c304bce9262f6721250e61de
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 11:46:33 +0100

    Add socket-io

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueSocketIO from "vue-3-socket.io";
+import SocketIO from "socket.io-client";
 
-createApp(App).use(store).use(router).mount("#app");
+const socket = window.socket || SocketIO("http://localhost:3000");
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(
+    new VueSocketIO({
+      debug: true,
+      connection: socket,
+    })
+  )
+  .mount("#app");
