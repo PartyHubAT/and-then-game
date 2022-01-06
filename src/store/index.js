d3703c3d67e86eb33f8c0cb9d52e2ad375e821e1
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 11:36:01 +0100

    Add logic to load default settings

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import { createStore } from "vuex";
 
+const settings =
+  window.settings || require("../../public/settings").defaultValues;
+
 export default createStore({
-  state: {},
+  state: {
+    settings: settings,
+  },
   mutations: {},
   actions: {},
   modules: {},
