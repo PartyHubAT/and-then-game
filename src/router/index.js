e7ad76b7c79772c63dd3d92292df9a6116d3b1de
ComradeVanti <kingvanti650@gmail.com>
2022-01-06 11:54:20 +0100

    Add text-writing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import TextWriting from "../views/TextWriting.vue";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "TextWriting",
+    component: TextWriting,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/TextWriting.vue
@@ -0,0 +1,11 @@
+﻿<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "TextWriting",
+};
+</script>
+
+<style scoped></style>
