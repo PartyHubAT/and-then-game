916bf1d095d887db5a26dc6ca697d9b44359df0d
ComradeVanti <kingvanti650@gmail.com>
2022-01-22 19:15:16 +0100

    Add logic to dynamically open connection to correct server

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from "./store";
 import VueSocketIO from "vue-3-socket.io";
 import SocketIO from "socket.io-client";
 
-const socket = window.socket || SocketIO("http://localhost:3000");
+const socket =
+  window.socket ||
+  SocketIO(`http://${window.location.hostname}:${process.env.VUE_APP_SERVER_PORT}`);
 
 createApp(App)
   .use(store)
