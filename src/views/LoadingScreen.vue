385a95a6bac86d94276e041839c46b6d601594e9
ComradeVanti <kingvanti650@gmail.com>
2022-01-26 09:05:44 +0100

    Add style for loading page

--- src/views/LoadingScreen.vue
+++ src/views/LoadingScreen.vue
@@ -1,5 +1,8 @@
 ﻿<template>
-  <div>Waiting for other players...</div>
+  <div id="content">
+    <div class="title">And then...</div>
+    <div class="subtitle">Waiting for players ⏲</div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +16,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.title {
+  font-family: var(--font-header), "Arial";
+  font-size: 70ex;
+  color: var(--primary);
+}
+.subtitle {
+  font-family: var(--font-content), "SansSerif";
+  color: #f8f8f8;
+  font-size: 15ex;
+}
+</style>
