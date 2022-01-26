b3a88cb9f8bd5306afac3e5bc3b18a465e088efe
ComradeVanti <kingvanti650@gmail.com>
2022-01-26 11:59:07 +0100

    Refactor project structure
    Move app style into app component

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,20 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
+  background: radial-gradient(
+    circle farthest-corner at top left,
+    var(--background) 0,
+    var(--background-dark) 100%
+  );
+}
+</style>
