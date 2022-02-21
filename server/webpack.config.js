846df132f798ea0951bf8a7365ef8041d27fe306
ComradeVanti <kingvanti650@gmail.com>
2022-02-21 12:40:17 +0100

    Refactor project structure
    Move server scripts into src subfolder

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./server.js",
+  entry: "./src/server.js",
   output: {
     filename: "server.js",
     path: path.resolve(__dirname, "../dist"),
